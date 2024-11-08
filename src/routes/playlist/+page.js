f971bd4c4ddd1097f40974341295d19343cd2621
Ted Steen <ted.steen@gmail.com>
2024-11-08 00:56:54 +0000

    cLeAnUp

--- src/routes/playlist/+page.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('./$types').PageLoad} */
-export async function load({ }) {
-    await new Promise((resolve) => {
-        resolve(undefined); //TODO: wait for playlist to be ready?
-    });
-
-    return {};
-}
